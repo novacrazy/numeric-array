4611222359bafc237c4698a3bf3268508eb3f758
novacrazy <novacrazy@gmail.com>
2018-03-29 20:52:31 -0500

    Move impls to their own file, simplify From implementation, and add SIMD Select trait

--- README.md
+++ README.md
@@ -10,25 +10,25 @@ Additionally, most of `num_traits` are implemented, including `Num` itself. So y
 Example:
 
 ```rust
-extern crate num_traits;
-#[macro_use]
-extern crate generic_array;
-#[macro_use]
-extern crate numeric_array;
+    extern crate num_traits;
+    #[macro_use]
+    extern crate generic_array;
+    #[macro_use]
+    extern crate numeric_array;
 
-use num_traits::Float;
-use numeric_array::NumericArray;
+    use num_traits::Float;
+    use numeric_array::NumericArray;
 
-fn main() {
-    let a = narr![f32; 1, 2, 3, 4];
-    let b = narr![f32; 5, 6, 7, 8];
-    let c = narr![f32; 9, 1, 2, 3];
+    fn main() {
+        let a = narr![f32; 1, 2, 3, 4];
+        let b = narr![f32; 5, 6, 7, 8];
+        let c = narr![f32; 9, 1, 2, 3];
 
-    // Compiles to a single vfmadd213ps instruction on my machine
-    let d = a.mul_add(b, c);
+        // Compiles to a single vfmadd213ps instruction on my machine
+        let d = a.mul_add(b, c);
 
-    assert_eq!(d, narr![f32; 14, 13, 23, 35]);
-}
+        assert_eq!(d, narr![f32; 14, 13, 23, 35]);
+    }
 ```
 
 When used with `RUSTFLAGS = "-C opt-level=3 -C target-cpu=native"`, then Rust and LLVM are smart enough to turn almost all operations into SIMD instructions, or even just evaluate them at compile time. The above example is actually evaluated at compile time, so if you were to view the assembly it would show the result only. Rust is pretty smart.
---
+++ src/simd.rs
@@ -0,0 +1,59 @@
+//! Context-sensitive SIMD operations
+
+use super::*;
+
+/// Selects elements from one array or another using `self` as a mask.
+pub unsafe trait Select<T, N: ArrayLength<T>> {
+    /// Selects elements from one array or another using `self` as a mask.
+    ///
+    /// Example:
+    ///
+    /// ```ignore
+    /// use simd::SimdSelect;
+    ///
+    /// let mask = narr![bool; true, false, false, true];
+    ///
+    /// let a = narr![i32; 1, 2, 3, 4];
+    /// let b = narr![i32; 5, 6, 7, 8];
+    ///
+    /// // Compiles to vblendvps on my machine
+    /// let selected = mask.select(a, b);
+    ///
+    /// assert_eq!(selected, narr![i32; 1, 6, 7, 4]);
+    /// ```
+    fn select(self, true_values: NumericArray<T, N>, false_values: NumericArray<T, N>) -> NumericArray<T, N>;
+}
+
+unsafe impl<T, N: ArrayLength<T>> Select<T, N> for NumericArray<bool, N>
+where
+    N: ArrayLength<bool>,
+{
+    fn select(self, true_values: NumericArray<T, N>, false_values: NumericArray<T, N>) -> NumericArray<T, N> {
+        let mut mask = ArrayConsumer::new(self.0);
+        let mut true_values = ArrayConsumer::new(true_values.0);
+        let mut false_values = ArrayConsumer::new(false_values.0);
+
+        let mut destination = ArrayBuilder::new();
+
+        for (dst, (m, (t, f))) in destination.array.iter_mut().zip(
+            mask.array
+                .iter()
+                .zip(true_values.array.iter().zip(false_values.array.iter())),
+        ) {
+            unsafe {
+                let t = ptr::read(t);
+                let f = ptr::read(f);
+                let m = ptr::read(m);
+
+                ptr::write(dst, if m { t } else { f });
+            }
+
+            destination.position += 1;
+            mask.position += 1;
+            true_values.position += 1;
+            false_values.position += 1;
+        }
+
+        NumericArray::new(destination.into_inner())
+    }
+}
